74aeda9327156264f5c397bd9feae92f8387707e
Goudmu <goudmu@gmail.com>
2024-05-29 09:25:42 +0700

    tambah api post untuk database posts

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -1,11 +1,25 @@
 import dbConnect from "@/lib/mongodb";
 import { Post } from "@/models/post";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async () => {
   try {
     await dbConnect();
     const post = await Post.find();
+    return NextResponse.json({ post });
+  } catch (error: any) {
+    console.log(error);
+    throw new Error();
+  }
+};
+export const POST = async (req: NextRequest) => {
+  try {
+    await dbConnect();
+    const { title, content } = await req.json();
+    const post = await Post.create({
+      title,
+      content,
+    });
     return NextResponse.json({ post });
   } catch (error: any) {
     console.log(error);
